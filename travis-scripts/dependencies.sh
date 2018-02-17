887a645aaf358a25ee010dd31dbd74a813f0f6cc
julien lopez <julien.lopez51@gmail.com>
2018-02-17 19:08:01 +0100

    trying to setup coveralls

--- travis-scripts/dependencies.sh
+++ travis-scripts/dependencies.sh
@@ -3,7 +3,8 @@
 git clone https://github.com/julienlopez/ETL
 git clone https://github.com/Microsoft/GSL
 
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get update -qq
 sudo apt-get update
 sudo apt-get install -y cmake libboost-all-dev
 sudo apt-cache search gcc
