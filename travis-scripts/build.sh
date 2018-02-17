49c91b34b756b298607b3ce9260575a9c280d8e1
julien lopez <julien.lopez51@gmail.com>
2018-02-17 19:32:38 +0100

    trying to setup coveralls

--- travis-scripts/build.sh
+++ travis-scripts/build.sh
@@ -2,12 +2,12 @@
 
 CC=gcc-7 && CXX=g++-7
 
+clang++ --version
+g++ --version
+gcov --version
+
 mkdir build
 cd build
 cmake ..
 make unit_tests
 ./unit_tests/unit_tests
-
-clang++ --version
-g++ --version
-gcov --version
