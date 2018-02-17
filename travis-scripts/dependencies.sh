a5d447c92aa7e6a2665cc32335f3237a3a5a4b42
julien lopez <julien.lopez51@gmail.com>
2018-02-17 12:43:09 +0100

    trying to fix build on clang

--- travis-scripts/dependencies.sh
+++ travis-scripts/dependencies.sh
@@ -3,5 +3,7 @@
 git clone https://github.com/julienlopez/ETL
 git clone https://github.com/Microsoft/GSL
 
+sudo apt-cache search clang
+
 sudo apt-get update
-sudo apt-get install -y clang-3.8 llvm-3.8-dev cmake libboost-all-dev
+sudo apt-get install -y cmake libboost-all-dev
