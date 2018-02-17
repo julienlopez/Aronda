b6c5cd23953452c15e9f951f3fa54c0e534c90b2
julien lopez <julien.lopez51@gmail.com>
2018-02-17 12:06:13 +0100

    setting up basic travis build

--- travis-scripts/dependencies.sh
+++ travis-scripts/dependencies.sh
@@ -3,7 +3,5 @@
 git clone https://github.com/julienlopez/ETL
 git clone https://github.com/Microsoft/GSL
 
-sudo apt-cache search boost
-
 sudo apt-get update
-sudo apt-get install -y clang-3.8 llvm-3.8-dev cmake
+sudo apt-get install -y clang-3.8 llvm-3.8-dev cmake libboost-all-dev
