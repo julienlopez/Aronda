91df73142bc36485ae901376074233f1bd068ad7
julien lopez <julien.lopez51@gmail.com>
2018-02-21 14:21:26 +0100

    trying to improve codecov

--- travis-scripts/dependencies.sh
+++ travis-scripts/dependencies.sh
@@ -8,4 +8,4 @@ sudo apt-get update -qq
 sudo apt-get update
 sudo apt-get install -y cmake libboost-all-dev gcc-7 g++-7 qt5-default qt5-qmake lcov
 
-sudo find /usr -name gcov*
+sudo update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-7 90
