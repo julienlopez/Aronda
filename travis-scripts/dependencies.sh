ed8caad43bcbe5572504ae611110a42f0cf9c8a0
julienlopez <julien.lopez51@gmail.com>
2018-02-21 11:07:08 +0100

    trying to set up codecov instead of coveralls

--- travis-scripts/dependencies.sh
+++ travis-scripts/dependencies.sh
@@ -7,4 +7,5 @@ sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
 sudo apt-get update
 sudo apt-get install -y cmake libboost-all-dev gcc-7 g++-7 qt5-default qt5-qmake
-# sudo apt-cache search qt5
+
+sudo apt-cache search lcov
