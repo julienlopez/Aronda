d78b2c038646215ae89b6fe5eb62ca5fc5512c01
julienlopez <julien.lopez51@gmail.com>
2018-02-21 11:17:11 +0100

    setting up codecov

--- travis-scripts/dependencies.sh
+++ travis-scripts/dependencies.sh
@@ -6,6 +6,4 @@ git clone https://github.com/Microsoft/GSL
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
 sudo apt-get update
-sudo apt-get install -y cmake libboost-all-dev gcc-7 g++-7 qt5-default qt5-qmake
-
-sudo apt-cache search lcov
+sudo apt-get install -y cmake libboost-all-dev gcc-7 g++-7 qt5-default qt5-qmake lcov
