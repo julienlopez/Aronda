1c9fbd2f55219999e52ab75f01ce921713c18181
julien lopez <julien.lopez51@gmail.com>
2018-02-22 12:10:04 +0100

    cleaning up squares definition building

--- gui/gamesquare.cpp
+++ gui/gamesquare.cpp
@@ -8,35 +8,65 @@
 namespace Aronda
 {
 
-	using boost::units::si::radians;
+using boost::units::si::radians;
 
-	GameSquareContainer_t buildSquares(const double outter_radius)
-	{
-		GameSquareContainer_t res;
-		res.push_back({ 0., outter_radius / 4., 0. * radians, 2. * PI });
-		for (int i = 0; i < 8; i++) // second circle for the center
-		{
-			const auto angle = (i + 0.5) / 4. * PI;
-			const auto delta = PI / 8.;
-			res.push_back({ outter_radius / 4, outter_radius / 2, angle - delta, angle + delta });
-		}
+namespace
+{
+    auto generateSecondRow(const double outter_radius)
+    {
+        GameSquareContainer_t res;
+        for(int i = 0; i < 8; i++) // second circle for the center
+        {
+            const auto angle = (i + 0.5) / 4. * PI;
+            const auto delta = PI / 8.;
+            res.push_back({outter_radius / 4, outter_radius / 2, angle - delta, angle + delta});
+        }
+        return res;
+    }
+    auto generateThirdRow(const double outter_radius)
+    {
+        GameSquareContainer_t res;
+        for(int i = 0; i < 8; i++) // third circle for the center
+        {
+            const auto angle = gsl::narrow<double>(i) * PI / 4.;
+            const auto delta = PI / 8.;
+            res.push_back({outter_radius / 2, 3 * outter_radius / 4, angle - delta, angle + delta});
+        }
+        return res;
+    }
+    auto generateFourthRow(const double outter_radius)
+    {
+        GameSquareContainer_t res;
+        for(int i = 0; i < 4; i++) // fourth circle for the center
+        {
+            const auto min_angle = (4 * i + 0.5) * PI / 8.;
+            const auto max_angle = (4 * i - 0.5) * PI / 8.;
+            res.push_back({3 * outter_radius / 4, outter_radius, min_angle, max_angle});
+            res.push_back({3 * outter_radius / 4, outter_radius, max_angle, (4 * i + 1.5) * PI / 8.});
+        }
+        return res;
+    }
 
-		for(int i = 0; i < 8; i++) // third circle for the center
-		{
-			const auto angle = gsl::narrow<double>(i) * PI / 4.;
-			const auto delta = PI / 8.;
-			res.push_back({ outter_radius / 2, 3 * outter_radius / 4, angle - delta, angle + delta });
-		}
+    template <class T> auto mergeVectors(std::vector<std::vector<T>> vectors)
+    {
+        std::vector<T> res;
+        for(auto& v : vectors)
+        {
+            for(auto& element : v)
+                res.push_back(std::move(element));
+        }
+        return res;
+    }
+}
 
-		for(int i = 0; i < 4; i++) // fourth circle for the center
-		{
-			const auto min_angle = (4 * i + 0.5) * PI / 8.;
-			const auto max_angle = (4 * i - 0.5) * PI / 8.;
-			res.push_back({ 3 * outter_radius / 4, outter_radius, min_angle, max_angle });
-			res.push_back({ 3 * outter_radius / 4, outter_radius, max_angle, (4 * i + 1.5) * PI / 8. });
-		}
-		Ensures(res.size() == c_number_of_squares);
-		return res;
-	}
-
+GameSquareContainer_t buildSquares(const double outter_radius)
+{
+    const GameSquare inner_circle{0., outter_radius / 4., 0. * radians, 2. * PI};
+    const auto res = mergeVectors<GameSquare>({{inner_circle},
+                                               generateSecondRow(outter_radius),
+                                               generateThirdRow(outter_radius),
+                                               generateFourthRow(outter_radius)});
+    Ensures(res.size() == c_number_of_squares);
+    return res;
+}
 }
--- gui/gamesquare.hpp
+++ gui/gamesquare.hpp
@@ -8,19 +8,19 @@
 namespace Aronda
 {
 
-	using Angle_t = boost::units::quantity<boost::units::si::plane_angle>;
+using Angle_t = boost::units::quantity<boost::units::si::plane_angle>;
 
-	struct GameSquare
-	{
-		double inner_radius;
-		double outter_radius;
-		Angle_t min_angle;
-		Angle_t max_angle;
-	};
+struct GameSquare
+{
+    double inner_radius;
+    double outter_radius;
+    Angle_t min_angle;
+    Angle_t max_angle;
+};
 
-	using GameSquareContainer_t = std::vector<GameSquare>;
+using GameSquareContainer_t = std::vector<GameSquare>;
 
-	GameSquareContainer_t buildSquares(const double outter_radius);
+GameSquareContainer_t buildSquares(const double outter_radius);
 
-	const auto PI = 3.14159265359 * boost::units::si::radians;
+const auto PI = 3.14159265359 * boost::units::si::radians;
 }
